e08c153bac8843079fe0d8754a6a76307a72c06f
slovenoya <ryanzhang880@gmail.com>
2022-10-10 12:35:14 -0700

    10/10:

--- ass3/RSA.py
+++ ass3/RSA.py
@@ -5,16 +5,24 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Util.number import getPrime
 
-LENGTH = 2048
+LENGTH = 16
 
 def get_prime(length: int) -> int:
   return getPrime(length)
 
+def inverse_mod(n: bytes, e: bytes, phi: bytes) -> bytes:
+  pass
+
+def generate_keys(e:bytes, length: int) -> 'tuple[int, int]':
+  p = get_prime(length)
+  q = get_prime(length)
+  n = p * q
+  phi = (p - 1) * (q - 1)
+  d = 0
+  return (n, d)
+
 def main():
   e = 65537
-  n = get_prime(LENGTH)
-  m = random.randint(0, n-1)
-  
 
 if __name__ == '__main__':
   main()
